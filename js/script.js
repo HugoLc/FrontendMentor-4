8d35d566a378e633c41e54728ddc58060197eacf
Hugo LC <hugolindoso.c@gmail.com>
2021-08-13 16:49:01 -0300

    Tratado o campo do valor da conta para valores negativos e incorretos

--- js/script.js
+++ js/script.js
@@ -9,7 +9,19 @@ var pessoas;
 
 function contaFocusOut(){
   valorConta.placeholder = '0';
-
+  var num = parseFloat(valorConta.value);
+  num = num.toFixed(2);
+  num = parseFloat(num);
+  if (num && num > 0) {
+    conta = num;
+    valorConta.value = conta;
+  }else if (num < 0) {
+    conta = parseFloat((num * (-1)).toFixed(2));
+    valorConta.value = conta;
+  }else {
+    valorConta.value = '';
+  }
+  console.log(conta);
 }
 
 
@@ -44,8 +56,8 @@ function qtdPessoasFocusOut(){
   if (qtdPessoas.value && qtdPessoas.value > 0) {
     pessoas = parseInt(qtdPessoas.value);
   }else if (qtdPessoas.value < 0) {
-    qtdPessoas.value = parseInt(qtdPessoas.value) * (-1);
-    pessoas = qtdPessoas.value;
+    qtdPessoas.value = parseInt(qtdPessoas.value * (-1));
+    pessoas = parseInt(qtdPessoas.value);
 
   }else {
     qtdPessoas.value = '';
