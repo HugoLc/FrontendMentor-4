8fc1eea179916c9de9860319575f1b1b41125dda
Hugo LC <hugolindoso.c@gmail.com>
2021-08-13 13:22:09 -0300

    Criado um if para tratar caso nãos eja digitado nada no campo

--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@ var custom;
 var pessoas;
 
 
+function contaFocusOut{
+  valorConta.placeholder = '0';
+}
+
+
 function customOnFocus(){
   customTip.placeholder = '';
   customTip.type = 'number';
@@ -16,15 +21,21 @@ function customFocusOut(){
   customTip.placeholder = 'Custom';
   customTip.type = 'text';
   texto = customTip.value;
-  custom = parseInt(texto);
-  console.log(custom);
-  texto = texto + '%';
+  if (texto) {
+    custom = parseInt(texto);
+    console.log(custom);
+    texto = texto + '%';
+  }
   customTip.value = texto;
 }
 
 function qtdPessoasFocusOut(){
   qtdPessoas.placeholder = '0';
-  qtdPessoas.value = convertToInt(qtdPessoas.value);
+  if (qtdPessoas.value) {
+    qtdPessoas.value = convertToInt(qtdPessoas.value);
+    pessoas = parseInt(qtdPessoas.value);
+    console.log(pessoas);
+  }
 }
 
 function convertToInt(valor){
